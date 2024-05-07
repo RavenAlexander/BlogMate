82b356a0748db63824c6422e797ff01b0963bbb1
Raven Alexander <theravenalexander@gmail.com>
2024-05-06 20:56:37 -0600

    adding dashboard and middleware

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 
 const express = require('express');
-const expressLayout = require('express-ejs-layouts')
+const expressLayout = require('express-ejs-layouts');
+const cookieParser = require('cookie-parser'); //saves session 
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 const connectDB = require('./server/config/db');
 
@@ -11,8 +14,21 @@ const PORT = 3000 || process.env.PORT;
 //Connect to DB
 connectDB();
 
+//Middleware
 app.use(express.urlencoded({extended: true}));
 app.use(express.json()); //allows us to pass data through forms
+app.use(cookieParser());
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+    }),
+    //cookie: {maxAge: new Date ( Date.now() + (3600000))}
+   
+}))
 
 app.use(express.static(__dirname + '/public'));
 //Templating Engine
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const Post = require('../models/Post')
+const Post = require('../models/Post');
+const User = require('../models/User');
+const bcrypt = require('bcrypt') // This encrypts and decrypts data for passwords
+const jwt = require('jsonwebtoken')
 
 const adminLayout = '../views/layouts/admin';
+const jwtSecret = process.env.JWT_SECRET;
+
+//Middleware -Check Login (This is a guard to make sure the user is logged in successfully)
+const authMiddleware = (req, res, next) => {
+    const token = req.cookies.token;
+
+    if(!token) {
+        return res.status(401).json({message: 'Unauthorized'});
+    }
+
+    try {
+        const decoded = jwt.verify(token, jwtSecret);
+        req.userId = decoded.userId;
+        next();
+    } catch(error) {
+        res.status(401).json({message: 'Unauthorized'});
+    }
+}
+
 //Routes
 // GET / Admin - Login Page
 router.get('/admin', async (req, res) => {
@@ -24,8 +46,62 @@ router.get('/admin', async (req, res) => {
 
 });
 
+//Routes
+// POST / Admin - Check Login
+router.post('/admin', async (req, res) => {
+    try {
+   const {username, password} = req.body;
+   const user = await User.findOne( {username} );
 
+   if(!user) {
+    return res.status(401).json({message: 'Invalid credentials'});
+   }
 
+   const isPasswordValid = await bcrypt.compare(password, user.password);
+
+   if(!isPasswordValid) {
+    return res.status(401).json({message: 'Invalid credentials'});
+   }
+
+   const token = jwt.sign({userId: user._id}, jwtSecret);
+   res.cookie('token', token, { httpOnly: true}); //this creates a randomly assigned web token
+
+   res.redirect('/dashboard');
+
+    } catch (error) {
+        console.log(error);
+    }
+
+});
+
+//POST / Admin Dash -Check Login (can't get into the dashboard unless logged in)
+router.get('/dashboard', authMiddleware, async (req, res) => {
+
+    res.render('admin/dashboard');
+});
+
+//Routes
+// POST / Admin - Register
+router.post('/register', async (req, res) => {
+    try {
+   const {username, password} = req.body;
+   const hashedPassword = await bcrypt.hash(password, 10);
+    
+    try {
+        const user = await User.create({ username, password:hashedPassword });
+        res.status(201).json({message: 'User Created', user });
+    } catch (error) {
+        if(error.code === 11000) {
+            res.status(409).json({message: 'User already in use'});
+        }
+        res.status(500).json({message: 'Internal server error'});
+    }
+
+    } catch (error) {
+        console.log(error);
+    }
+
+});
 
 
 module.exports = router;
